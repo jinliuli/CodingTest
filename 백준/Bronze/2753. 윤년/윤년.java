1d497ac73b81c113712bbd9e8102844670971f71
jinliuli <jinliuli0305@gmail.com>
2024-12-23 18:38:23 +0900

    [Bronze V] Title: 윤년, Time: 172 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		int num = 0;
+		num = scan.nextInt();
+		
+		if ((num % 4 == 0)
+				&& ((num % 100 != 0) ||(num % 400 == 0))) {
+			System.out.println("1");
+		} else {
+			System.out.println("0");
+		}
+		
+	}
+	
+}
